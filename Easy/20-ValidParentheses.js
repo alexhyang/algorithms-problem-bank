ad8fed08a741a8889d17450761481c7f2cc34d2f
Alex Yang <alex.hyang.dev@gmail.com>
2021-11-14 00:00:34 -0800

    added problem description

--- Easy/20-ValidParentheses.js
+++ Easy/20-ValidParentheses.js
@@ -1,3 +1,13 @@
+/* 
+Given a string s containing just the characters '(', ')', 
+'{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+*/
+
 const isValid = function (s) {
   var stack = [];
   for (const p of s) {
