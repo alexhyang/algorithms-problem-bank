2301ee4a031bc26611d3c4eedb932355ccd1a4ba
Alex Yang <alex.hyang.dev@gmail.com>
2023-08-31 21:57:18 -0700

    set up prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:jest/recommended",
     "plugin:jest/style",
+    "prettier",
   ],
   ignorePatterns: [
     '.eslintrc.js',
